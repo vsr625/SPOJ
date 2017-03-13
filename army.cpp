8decbcf023f8c451cbccfd5f8d99dd21dd3da349
V. Subramani Raju <vsr625@gmail.com>
2017-03-13 12:26:15 +0530

    army.cpp

---
+++ army.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int cases;
+	cin>>cases;
+	while (cases--) {
+		int n, m;
+		cin>>n>>m;
+		int temp;
+		int maxg=0, maxm=0;
+		for (int i = 0; i < n ; i++) {
+			cin>>temp;
+			if(temp > maxg) maxg=temp;
+		}
+		for (int i = 0; i < m ; i++) {
+			cin>>temp;
+			if(temp > maxm) maxm=temp;
+		}
+		if(maxm > maxg)
+			cout << "MechaGozilla" << "\n";
+		else
+			cout << "Godzilla" << "\n";
+	}
+	return 0;
+}
